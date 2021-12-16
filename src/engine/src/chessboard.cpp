9b39729c4f05283193966529508cc5bcab0a77ab
Alexander Loodin Ek <alexander.loodin.ek@gmail.com>
2021-12-15 18:18:16 -0800

    Building notation tests and setting up initial board

--- src/engine/src/chessboard.cpp
+++ src/engine/src/chessboard.cpp
@@ -1,16 +1,76 @@
 ﻿#include "chessboard.h"
 #include <cstring>
+#include <utility>
+
+Notation&& Notation::BuildPosition(byte file, byte rank)
+{
+	Notation result(0xF, 0xF);
+	byte corrFile = (byte)(tolower(file) - 'a');
+	byte corrRank = rank - 1;
+
+	// validate placement is inside the board.
+	if (corrFile > 7 || corrRank > 7)
+		return std::move(result);
+
+	result.file = corrFile;	
+	result.rank = corrRank;
+	return std::move(result);
+}
+
+ChessboardTile::ChessboardTile(Notation&& notation) :
+	m_position(std::move(notation))
+{
+}
+
+bool ChessboardTile::operator==(const ChessboardTile& rhs) const
+{
+	bool result = m_position == rhs.m_position;
+	result |= m_piece == rhs.m_piece;
+	return result;
+}
+
+bool Notation::operator==(const Notation& rhs) const
+{
+	bool result = rank == rhs.rank;
+	result |= file == rhs.file;	
+	return result;
+}
 
 Chessboard::Chessboard()
 {
+	for (byte r = 0; r < 8; r++)
+	{
+		for (byte f = 0; f < 8; f++)
+		{
+			Notation pos(r,f);
+			m_tiles[pos.getIndex()].editPosition() = std::move(pos);
+		}
+	}
 }
 
 Chessboard::Chessboard(const Chessboard& other)
 {
-	std::memcpy(m_tiles, other.m_tiles, sizeof(ChessboardTile) * 64);
+	std::memcpy(m_tiles, other.m_tiles, sizeof(m_tiles));
+}
+
+const ChessboardTile& Chessboard::getTile(const Notation& position) const
+{
+	return m_tiles[position.getIndex()];
+}
+
+ChessboardTile& Chessboard::editTile(const Notation& position)
+{
+	return m_tiles[position.getIndex()];
 }
 
 void Chessboard::Clear()
 {
 	std::memset(m_tiles, 0, sizeof(m_tiles));
 }
+
+bool Chessboard::PlacePiece(ChessPiece p, const Notation& tile)
+{
+	m_tiles[tile.getIndex()].editPiece() = std::move(p);
+	return false;
+}
+
--- test/src/chessboard_test.cpp
+++ test/src/chessboard_test.cpp
@@ -19,7 +19,35 @@ TEST_F(ChessboardFixture, Empty)
 {
     Chessboard b; // by default a board should start empty.
     ChessboardTile emptyTile;
-    EXPECT_EQ(emptyTile, b.getTile("a8"));
+    EXPECT_EQ(emptyTile, b.getTile({ 0, 0 }));
+
+    ChessboardTile expected(Notation::BuildPosition('h', 8));
+    
+    EXPECT_EQ(expected, b.getTile({ 7, 7 }));
+
+    expected = Notation::BuildPosition('d', 4);
+    EXPECT_EQ(expected, b.getTile({ 3, 3 }));
+
+    expected = Notation::BuildPosition('e', 5);
+    EXPECT_EQ(expected, b.getTile({ 4, 4 }));
+}
+
+TEST_F(ChessboardFixture, Notation_BuildPosition)
+{
+    Notation expected(0, 0);
+    auto pos = Notation::BuildPosition('a', 1);
+    EXPECT_EQ(expected, pos);
+
+    expected = Notation(0xf, 0xf);
+    pos = Notation::BuildPosition('z', 1); // invalid position
+    EXPECT_EQ(expected, pos);
+
+    Notation defaultValue;
+    EXPECT_EQ(expected, defaultValue);
+
+    expected = Notation(4, 3);
+    pos = Notation::BuildPosition('e', 4);
+    EXPECT_EQ(expected, pos);
 }
 
 ////////////////////////////////////////////////////////////////
