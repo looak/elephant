5a78a2693c1770ca105e4323a1debbda17a19ff9
Alexander Loodin Ek <alexander.loodin.ek@gmail.com>
2022-03-29 17:58:46 -0700

    removed tile compare

---
+++ test/src/unmake_test.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+
+namespace ElephantTest
+{
+////////////////////////////////////////////////////////////////
+class UnmakeFixture : public ::testing::Test
+{
+public:
+    /*   virtual void SetUp()
+    {
+
+    };
+    virtual void TearDown() {};
+*/
+};
+////////////////////////////////////////////////////////////////
+}
