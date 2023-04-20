83bac78a543cdc8748aa1a4f38f133ff56850388
Alexander Loodin Ek <alexander.loodin.ek@gmail.com>
2023-04-20 01:20:58 -0700

    0.1.0-alpha.0

--- src/cli/src/elephant_cli.cpp
+++ src/cli/src/elephant_cli.cpp
@@ -16,8 +16,8 @@
 Application::Application()
 {
 	MESSAGE() << " Elephant Gambit Open Source Chess Engine 2021-2023"; // EGOSCE
-	MESSAGE() << " Versions: \tCLI: " << ELEPHANT_CLI_VERSION_STR << ELEPHANT_CLI_VERSION_SUFFIX <<
-		 "\tEngine: " << ELEPHANT_GAMBIT_VERSION_STR << ELEPHANT_GAMBIT_VERSION_SUFFIX;	
+	MESSAGE() << " versions:\n   cli:    " << ELEPHANT_CLI_VERSION_STR << "-" << ELEPHANT_CLI_VERSION_PRERELEASE <<  ELEPHANT_CLI_VERSION_SUFFIX <<
+		 "\n   engine: " << ELEPHANT_GAMBIT_VERSION_STR << "-" << ELEPHANT_GAMBIT_VERSION_PRERELEASE << ELEPHANT_GAMBIT_VERSION_SUFFIX;
 	MESSAGE() << " Source: https://github.com/looak/elephant";
 	MESSAGE() << " Author: Alexander Loodin Ek\n";
 }
