7ddfe3a254ec672d15cfbf2cf33c460668710cd0
Alex Ek (BABYLON) <alexander.loodin.ek@gmail.com>
2025-08-21 22:15:32 -0700

    Generated header comments for all headers
    Generated small brief for each header.

--- src/engine/inc/move/generation/king_pin_threats.hpp
+++ src/engine/inc/move/generation/king_pin_threats.hpp
@@ -1,3 +1,27 @@
+/******************************************************************************
+ * Elephant Gambit Chess Engine - a Chess AI
+ * Copyright(C) 2025  Alexander Loodin Ek
+ * 
+ * This program is free software : you can redistribute it and /or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.If not, see < http://www.gnu.org/licenses/>. 
+ *****************************************************************************/
+
+/**
+ * @file king_pin_threats.hpp
+ * @brief Implements functionality to assess king pin threats and check conditions in a chess game 
+ * 
+ */
+
 #pragma once
 
 #include <bitboard/bitboard.hpp>
--- src/engine/inc/position/en_passant_state_info.hpp
+++ src/engine/inc/position/en_passant_state_info.hpp
@@ -1,3 +1,27 @@
+/******************************************************************************
+ * Elephant Gambit Chess Engine - a Chess AI
+ * Copyright(C) 2025  Alexander Loodin Ek
+ * 
+ * This program is free software : you can redistribute it and /or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.If not, see < http://www.gnu.org/licenses/>. 
+ *****************************************************************************/
+
+/**
+ * @file en_passant_state_info.hpp
+ * @brief Handles the en passant state information for a chess game 
+ * 
+ */
+
 #pragma once
 #include <defines.hpp>
 #include <bitboard/bitboard.hpp>
